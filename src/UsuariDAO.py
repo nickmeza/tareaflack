a208736dc94a32fbc48acb1e32317a498ab2ae2a
Nick Carlos Mesias Rivera <nickmeza@upeu.edu.pe>
2020-05-18 19:33:09 -0500

    nick

--- src/UsuariDAO.py
+++ src/UsuariDAO.py
@@ -1,8 +1,8 @@
 import pymysql
 from conexion import Conexion
 from sqlalchemy import create_engine
-from json import decoder
-cx=Conexion()
+from json import dumps
+cx = Conexion()
 
 class Usuario:
     idusuario = 0
@@ -11,16 +11,67 @@ class Usuario:
     estado = ""
     def readAll(self):
         try:
-            sql = "select * from USUARIO"
             conexion=cx.conecta()
             cursor = conexion.cursor(pymysql.cursors.DictCursor)
-            cursor.execute(sql)
-            '''lista = cursor.fetchall()'''
-            for row in cursor:
-                return row
+            cursor.callproc('listar_usuario')
+            rows = cursor.fetchall()
+            return rows
+        except Exception as e:
+            print(e)
         finally:
             conexion.close()
-
-
-obj = Usuario()
-print(obj.readAll())
+    def delete(self):
+        try:
+            conexion=cx.conecta()
+            cursor=conexion.cursor(pymysql.cursors.DictCursor)
+            cursor.callproc('delete_usuario',[self.idusuario])
+            conexion.commit()
+            return 1
+        except Exception as e:
+            print(e)
+        finally:
+            cursor.close()
+            conexion.close()
+    def agregarusuario(self):
+        nomuser=self.nomuser
+        clave=self.clave
+        data=[nomuser,clave]
+        try:
+            conexion=cx.conecta()
+            cursor=conexion.cursor(pymysql.cursors.DictCursor)
+            cursor.callproc("create_usuario",data)
+            conexion.commit()
+            return 1
+        except Exception as e:
+            print(e)
+        finally:
+            cursor.close()
+            conexion.close()
+    def buscarusuario(self):
+        try:
+            conexion=cx.conecta()
+            cursor=conexion.cursor(pymysql.cursors.DictCursor)
+            cursor.callproc('read_usuario',[self.idusuario,])
+            rows=cursor.fetchall()
+            return rows
+        except Exception as e:
+            print(e)
+        finally:
+            cursor.close()
+            conexion.close()
+    def modificarusuario(self):
+        try:
+            idu=self.idusuario
+            nomuser=self.nomuser
+            clave=self.clave
+            data=[idu,nomuser,clave]
+            conexion=cx.conecta()
+            cursor=conexion.cursor(pymysql.cursors.DictCursor)
+            cursor.callproc("update_usuario",data)
+            conexion.commit()
+            return 1
+        except Exception as e:
+            print(e)
+        finally:
+            cursor.close()
+            conexion.close()
--- src/app.py
+++ src/app.py
@@ -1,12 +1,78 @@
-from flask import Flask
+from flask import Flask, jsonify, request, make_response
+from flask_restful import Resource, Api, reqparse
+from sqlalchemy import create_engine
+from json import dumps
 from UsuariDAO import Usuario
+user = Usuario()
 app = Flask(__name__)
-user=Usuario()
-@app.route("/list",methods=['GET'])
-def hello():
-    print(user.readAll())
-    return user.readAll()
+
+
+@app.route('/', methods=['GET'])
+def listar():
+    return jsonify({'mensaje': 'Bienvenidos a Flask'})
+
+
+@app.route('/usuario/listar', methods=['GET'])
+def users():
+    try:
+        rows = user.readAll()
+        respuesta = jsonify(rows)
+        respuesta.status_code = 200
+        return respuesta
+    except Exception as e:
+        print(e)
+
+
+@app.route('/usuario/buscar/<int:id>')
+def buscarescuela(id):
+	try:
+		user.idusuario = id
+		row = user.buscarusuario()
+		resp = jsonify(row)
+		resp.status_code = 200
+		return resp
+	except Exception as e:
+		print(e)
+
+
+@app.route('/usuario/create', methods=['POST'])
+def agregarusuario():
+    try:
+        _json = request.json
+        user.nomuser=_json['nomuser']
+        user.clave=_json['pass']
+        if request.method=='POST':
+            resp=user.agregarusuario()
+            resp=jsonify('USUARIO')
+            resp.status_code=200
+        return resp
+    except Exception as e:
+        print(e)
+
+@app.route('/usuario/eliminar/<int:id>', methods=['GET'])
+def eliminarusuario(id):
+    try:
+        user.idusuario=id
+        resp=user.delete()
+        resp=jsonify('Usuario Elimindado')
+        resp.status_code=200
+        return resp
+    except Exception as e:
+        print(e)    
+@app.route('/usuario/modificar', methods=['PUT'])
+def modificarusuario():
+    try:
+        _json=request.json
+        user.nomuser=_json['nomuser']
+        user.clave=_json['pass']
+        user.idusuario=_json['iduser']
+        if request.method == 'PUT':
+            resp = user.modificarusuario()
+            resp = jsonify('Usuario Modificada')
+            resp.status_code=200
+            return resp
+    except Exception as e:
+        print(e)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=4000,debug=True)
-
+    app.run(host="localhost", port=5000, debug=True)    
